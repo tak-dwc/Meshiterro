a40343a7d0349723e3fee04ed4ab22c184055c43
taku3277@gmail.com <ec2-user@ip-172-31-32-148.ec2.internal>
2021-07-11 12:24:50 +0000

    [Start]4-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -23,6 +23,7 @@ class PostImagesController < ApplicationController
     @post_image=PostImage.find(params[:id])
     @post_image.destroy
     redirect_to post_images_path
+    
   end
   
   private
