e5b2cf9ff74b92d02588834f851c9193bdd7b21b
taku3277@gmail.com <ec2-user@ip-172-31-32-148.ec2.internal>
2021-07-11 10:41:38 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '83f1b31f38ddb1383229a3fb6ab48b781cdf7255fdb58396655dd0b633507573ca9fa7183c987469fb0135a9c315b91a9f57d174209575ac9c6d2a964ccdf279'
